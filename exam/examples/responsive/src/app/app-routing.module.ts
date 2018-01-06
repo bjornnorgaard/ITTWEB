1fd9ffe7e568a08cbfb67ed2c37a4956875cb469
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2018-01-06 19:35:14 +0100

    Demo done

--- exam/examples/responsive/src/app/app-routing.module.ts
+++ exam/examples/responsive/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BootstrapComponent} from './bootstrap/bootstrap.component';
+import {CssgridComponent} from './cssgrid/cssgrid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BootstrapComponent},
+  {path: 'bootstrap', component: BootstrapComponent},
+  {path: 'cssgrid', component: CssgridComponent},
+  {path: '**', component: BootstrapComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- exam/examples/responsive/src/app/app.module.ts
+++ exam/examples/responsive/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BootstrapComponent } from './bootstrap/bootstrap.component';
+import { CssgridComponent } from './cssgrid/cssgrid.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { AdsComponent } from './components/ads/ads.component';
+import { ImageComponent } from './components/image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BootstrapComponent,
+    CssgridComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    BodyComponent,
+    AdsComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
